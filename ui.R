a667d6cd6e942b0cbd6c7236dd2a7337ff768377
alexpetralia <alex.petralia@gmail.com>
2015-08-30 19:02:41 -0400

    updates

--- ui.R
+++ ui.R
@@ -30,17 +30,17 @@ shinyUI(navbarPage(img(src = 'title.png', style='padding-bottom: 10px'), id="nav
            absolutePanel(id ="controls", class ="panel panel-default", fixed=TRUE,
                          draggable=TRUE, top=60, left='auto', right=20, bottom='auto',
                          width=300, height='auto',
-                         checkboxGroupInput("status", h4("Business License Status"), bstatus,
+                         checkboxGroupInput("status", h4("Business license status"), bstatus,
                                             selected = "Active"),
-                         checkboxGroupInput("violations", h4("Violation"), violationType, 
-                                            selected="Rodents"),
-                         sliderInput('period', h4("Select Time Period"),
+                         checkboxGroupInput("violations", h4("Violation type"), violationType, 
+                                            selected="Unsafe food preparation"),
+                         sliderInput('period', h4("Select your time period"),
                                      min = 2008,
                                      max = 2015,
                                      value = c(2008,2008),
-                                     sep="", step=1, ticks=FALSE,animate=animationOptions(loop=T), round=TRUE),
+                                     sep="", step=1, ticks=FALSE,animate=animationOptions(interval=2000, loop=T), round=TRUE),
                          tags$script("$(document).ready(function(){
-                                      setTimeout(function() {$('.slider-animate-button').click()},15000);
+                                      setTimeout(function() {$('.slider-animate-button').click()},12000);
                                      });")
                         )
            )
@@ -52,9 +52,9 @@ shinyUI(navbarPage(img(src = 'title.png', style='padding-bottom: 10px'), id="nav
                           code("returns random 100 rows"),
                           br(),
                           br(),
-                          checkboxGroupInput("status2", h4("Business License Status"), bstatus,
+                          checkboxGroupInput("status2", h4("Business license status"), bstatus,
                                              selected = "Active"),
-                          checkboxGroupInput("violations2", h4("Violation"), violationType, 
+                          checkboxGroupInput("violations2", h4("Violation type"), violationType, 
                                              selected=violationType)
              ),
              mainPanel(
@@ -73,11 +73,11 @@ shinyUI(navbarPage(img(src = 'title.png', style='padding-bottom: 10px'), id="nav
                p("We used the given latitude and longitudes to build the map. However, many of these were not given although a complete address was. To overcome this, we passed the addresses without coordinates through Google\'s", strong("Geocoding API"), "to resolve the coordinates. For the few records that had neither addresses nor coordinates, we dropped."),
                p("For the map, we filtered the dataset for only notable violation types - for example, many were signage or labeling violations and they crowded the results. However, the entire dataset is used in the", em("free text search.")),
                p("We manually created the \"Violations\", which were not in the original dataset. We used a naming crosswalk to link certain violation types to our fields. The crosswalk is as follows:"),
-               tags$li(strong('Cockroaches'), "is composed of a keyword search on \"cockroach\" in the \"Insects Rodents Animals\" category."),
-               tags$li(strong('Rodents'), "is composed of a keyword search on \"dropping\" in the \"Insects Rodents Animals\" category."),
-               tags$li(strong('Unhygienic'), "is composed of the categories \"Adequate Handwashing/Where/When/How\", \"Dishwashng Facilities\", \"Food Contact Surfaces Clean\", \"Good Hygienic Practices\", \"Location  Accessible\", \"Prevention of Contamination from Hands\", \"Separation/Sanitizer Criteria\", \"Toilet Enclosed Clean\"."),
-               tags$li(strong('Unsafe food preparation'), "is composed of \"Cold Holding\", \"Cooking Temperatures\", \"Hot Holding\", \"PHF\'s Properly Thawed\", \"Reheating\", \"Separation  Segregation Cross Contamination\", \"Spoilage Unsafe Food\", \"Washing fruits and veg\'s.\"."),
-               tags$li(strong('Unlicensed pesticide use'), "is composed of \"Pesticide Usage\"."),
+               tags$li(em('Cockroaches'), "is composed of a keyword search on \"cockroach\" in the \"Insects Rodents Animals\" category."),
+               tags$li(em('Rodents'), "is composed of a keyword search on \"dropping\" in the \"Insects Rodents Animals\" category."),
+               tags$li(em('Unhygienic'), "is composed of the categories \"Adequate Handwashing/Where/When/How\", \"Dishwashng Facilities\", \"Food Contact Surfaces Clean\", \"Good Hygienic Practices\", \"Location  Accessible\", \"Prevention of Contamination from Hands\", \"Separation/Sanitizer Criteria\", \"Toilet Enclosed Clean\"."),
+               tags$li(em('Unsafe food preparation'), "is composed of \"Cold Holding\", \"Cooking Temperatures\", \"Hot Holding\", \"PHF\'s Properly Thawed\", \"Reheating\", \"Separation  Segregation Cross Contamination\", \"Spoilage Unsafe Food\", \"Washing fruits and veg\'s.\"."),
+               tags$li(em('Unlicensed pesticide use'), "is composed of \"Pesticide Usage\"."),
                br(),
                h4("Sources"),
                p("All data was downloaded from", a("the city of Boston's public data repository.", href="https://data.cityofboston.gov/Health/Food-Establishment-Inspections/qndu-wx8w")),
